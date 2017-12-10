6a17aff1b415efe35dcd33727ec1113d0e660150
splinterific <markgpreston@gmail.com>
2017-12-10 18:32:57 +0000

    working for the output file, cleaning paths to be relative

--- CA1/CA1/Program.cs
+++ CA1/CA1/Program.cs
@@ -16,31 +16,7 @@ namespace CA1
 
         public static void Main(string[] args)
         {
-
-            //string[] lines = System.IO.File.ReadAllLines(pathIn);
-            //int count = 0;
-            //foreach (string line in lines)
-            //{
-            //    if (line == "------------------------------------------------------------------------")
-            //    {
-            //        count += 1;
-
-
-            //        // in here we'll take the next line and run it through a split based on the 
-            //        // " | " 
-            //        // string[] tokens = str.Split(new[] { "is Marco and" }, StringSplitOptions.None);
-            //        // this will give us an array we can apply to the class
-
-            //    }
-            //    else {
-            //        count +=0;
-            //    }
-            //}
-            //Console.WriteLine(count);
-            //// Keep the console window open in debug mode.
-            //Console.WriteLine("Press any key to exit.");
-            //System.Console.ReadKey();
-
+            Console.WriteLine("Please hold, working!");
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "commit_changes.txt");
@@ -48,14 +24,27 @@ namespace CA1
             string splitString = "------------------------------------------------------------------------\n";
             string[] fileLines = fileContents.Split(new string[] { splitString }, StringSplitOptions.RemoveEmptyEntries);
 
+
             foreach (var line in fileLines)
             {
                 string[] commitLines = line.Split(new string[] { " | " }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var i in commitLines)
+                using (StreamWriter file =
+                           new StreamWriter(@"/Users/mpreston/Documents/code/college/Advanced-Web-Programming/CA1/CA1/output.csv"))
                 {
-                    Console.WriteLine(i);
+                    foreach (string s in fileLines)
+                    {
+                        
+                        string l = s.Replace(",", "").Replace(" | ", ",").Replace("\n", "-").Replace("   ", " ");
+                        file.WriteLine(l);
+
+                    }
+
                 }
+
+
+
             }
+            Console.WriteLine("Finished");
 
         }
 
