6154b3ef74dad9dbeb7ca13a128cca1c18bb2e2d
splinterific <markgpreston@gmail.com>
2018-01-14 11:12:28 +0000

    updated to add addition, subtraction and multiplication

---
+++ CA3/provided files/calculator_app_original.py
@@ -0,0 +1,8 @@
+from calculator_original import Calculator
+
+my_calc = Calculator()
+value = ''
+while value != 'E':
+   value = raw_input('Press Number or E to Finish: ')
+   my_calc.input(value)
+   print my_calc.getOutput()
---
+++ CA3/solution files/calculator.py
@@ -0,0 +1,48 @@
+class Calculator:
+
+    def __init__(self):
+        self.__input = '0'
+        self.__operation = ''
+
+    def add(self, first, second):
+        return first + second
+
+    def multiply(self, first, second):
+        return first * second
+
+    def subtract(self, first, second):
+        return first - second
+
+    def input(self, value):
+        if value == '#':
+            self.__input = '0'
+
+
+        # valid operators
+        elif value == '+':
+            self.__operation = value
+        elif value == '-':
+            self.__operation = value
+        elif value == '*':
+            self.__operation = value
+
+
+        # values to be shown after operator
+        elif self.__operation != '':
+            if self.__operation == '+':
+                self.__input = str(self.add(int(self.__input), int(value)))
+            elif self.__operation == '-':
+                self.__input = str(self.subtract(int(self.__input), int(value)))
+            elif self.__operation == '*':
+                self.__input = str(self.multiply(int(self.__input), int(value)))
+            else:
+                return "invalid operator"
+
+        else:
+            self.__input += str(value)
+        #print self.__input
+
+    def getOutput(self):
+        if self.__input == '0E':
+            return 0
+        return int(self.__input)
