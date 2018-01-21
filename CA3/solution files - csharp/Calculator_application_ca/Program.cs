cd005d970254640c684c7d992cc119c92bc909e8
splinterific <markgpreston@gmail.com>
2018-01-21 13:36:24 +0000

    addedd the first c# files to get a basic set working

---
+++ CA3/solution files - csharp/Calculator_application_ca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator_application_ca
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            calculator sq = new calculator();
+            Console.WriteLine(sq.Square(12));  
+                Console.WriteLine(sq.Add(8, 63));  
+                Console.WriteLine(sq.Multiply(5, 8));  
+                Console.WriteLine(sq.Subtract(10, 42));  
+                Console.WriteLine(sq.Division(3, 4));  
+                Console.ReadLine();  
+
+        }
+    }
+}
---
+++ CA3/solution files - csharp/Calculator_application_ca/calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Calculator_application_ca
+{
+    public class calculator
+    {
+        
+            // Square function   
+    public int Square(int num)  
+    {  
+        return num * num;  
+    }  
+    // Add two integers and returns the sum  
+    public int Add(int num1, int num2)  
+    {  
+        return num1 + num2;  
+    }  
+  
+    // Multiply two integers and retuns the result  
+    public int Multiply(int num1, int num2)  
+    {  
+        return num1 * num2;  
+    }  
+    // Subtracts small number from big number  
+    public int Subtract(int num1, int num2)  
+    {  
+            if (num1 > num2)  
+            {  
+                return num1 - num2;  
+            }  
+  
+            return num2 - num1;  
+  
+        }  
+        //performing Division on two float variables.  
+    public float Division(float num1, float num2)   
+    {  
+        return num1 / num2;  
+    }  
+
+
+    }
+}
