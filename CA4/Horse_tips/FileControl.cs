150f47380612ae975f7dd0a27d36275f175834d3
splinterific <markgpreston@gmail.com>
2018-02-22 15:30:02 +0000

    updated class

---
+++ CA4/Horse_tips/FileControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MongoDB.Bson;
+namespace Horse_tips
+{
+    public static class FileControl
+    {
+        public static string FileGrab()
+        {
+            Console.WriteLine("Please hold, working!");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "../../data.csv");
+            return fileName;
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static string[] ParseFile(string contents)
+        {
+            string splitString = "\n";
+            string[] fileLines = contents.Split(new string[] { splitString }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in fileLines)
+            {
+                string l = line.Replace("(", "").Replace(")", ",").Replace(" ", "");
+                string[] entry = l.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                // take each entry and do something
+                string CourseName = entry[0];
+                string DateRan = entry[1] + "-" + entry[2] + "-" + entry[3];
+                string AmountWon = entry[4];
+                string Result = entry[5];
+
+                BsonDocument docu = new BsonDocument{
+                    {"CourseName", CourseName},
+                    {"DateRan", DateRan},
+                    {"AmountWon", AmountWon},
+                    {"Result", Result}
+                };
+                DBConnectionClass.DbConnect(docu);
+                Console.WriteLine(CourseName + " Added");
+            }
+            return fileLines;
+
+        }
+    }
+}
--- CA4/Horse_tips/Program.cs
+++ CA4/Horse_tips/Program.cs
@@ -13,55 +13,9 @@ namespace Horse_tips
     {
         public static void Main(string[] args)
         {
-            string outname = FileGrab();
-            string fileContents = ReadFile(outname);
-            string[] output = ParseFile(fileContents); 
+            string outname = FileControl.FileGrab();
+            string fileContents = FileControl.ReadFile(outname);
+            string[] output = FileControl.ParseFile(fileContents); 
         }
-
-        public static string ReadFile(string fileName)
-        {
-            using (var reader = new StreamReader(fileName))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-
-        public static string[] ParseFile(string contents)
-        {
-            string splitString = "\n";
-            string[] fileLines = contents.Split(new string[] { splitString }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var line in fileLines)
-            {
-                string l = line.Replace("(", "").Replace(")", ",").Replace(" ", "");
-                string[] entry = l.Split(new string[] { ","}, StringSplitOptions.RemoveEmptyEntries);
-                // take each entry and do something
-                string CourseName = entry[0];
-                string DateRan = entry[1] + "-" + entry[2] + "-" + entry[3];
-                string AmountWon = entry[4];
-                string Result = entry[5];
-
-                BsonDocument docu = new BsonDocument{
-                    {"CourseName", CourseName},
-                    {"DateRan", DateRan},
-                    {"AmountWon", AmountWon},
-                    {"Result", Result}
-                };
-                DBConnectionClass.DbConnect(docu);
-                Console.WriteLine(CourseName + " Added");
-            }
-            return fileLines;
-
-        }
-
-        public static string FileGrab()
-        {
-            Console.WriteLine("Please hold, working!");
-            string currentDirectory = Directory.GetCurrentDirectory();
-            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            var fileName = Path.Combine(directory.FullName, "../../data.csv");
-            return fileName;
-        }
-
     }
 }
